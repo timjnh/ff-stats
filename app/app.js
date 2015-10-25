e499c3474bbc616549aa6fa42888fb0e789b7c60
Tim Jones <tim.jones@meltwater.com>
2015-10-24 21:21:18 -0400

    Using express rather than http-server

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
-angular.module('myApp', ['myApp.statsChart']);
+var express = require('express');
+
+var app = express();
+//app.use(express.logger());
+
+/*app.configure(function(){
+    app.set('views', __dirname);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/'));
+    app.use(app.router);
+    app.engine('html', require('ejs').renderFile);
+});*/
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(request, response) {
+    response.render('index.html')
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
