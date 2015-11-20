8bbab2c0e25d5ad2e4f9135dcbbf0bf526c688cd
Tim Jones <tim.jones@meltwater.com>
2015-11-20 15:12:10 -0500

    Allowing the list of inputs to be specified on the command line

--- src/build_networks.js
+++ src/build_networks.js
@@ -23,17 +23,35 @@ args = require('yargs')
     .describe('strategy', 'Network strategy to use')
     .choices('strategy', networkStrategyFactory.getStrategyNames())
     .default('strategy', PerceptronStrategy.NAME)
+    .array('input')
+    .describe('input', 'Input name or list of inputs to use when building networks')
     .boolean('inputs-only')
     .describe('inputs-only', 'If set, inputs will be calculated by not networks')
     .boolean('force-input-calc')
     .describe('force-input-calc', 'If set, inputs will be calculated regardless of whether they have already been calculated')
     .argv;
 
+function getInputsForPlayer(player) {
+    var skippedInputs,
+        inputs = inputsService.getInputsListForPosition(player.position);
+
+    if(args.input) {
+        inputs = _.intersection(inputs, args.input);
+        skippedInputs = _.difference(args.input, inputs);
+
+        if(skippedInputs.length) {
+            console.log('Skipping ' + skippedInputs.join(', ') + ' for "' + player.name + '" of the ' + player.team + ' because they do not apply to his position (or are not valid)');
+        }
+    }
+
+    return inputs;
+}
+
 function buildAndSaveInputsForPlayer(player) {
     var inputsPromiseChain = q.when(player);
     player.games.forEach(function(playerGame) {
         inputsPromiseChain = inputsPromiseChain.then(function getInputsForPlayerAndGame(updatedPlayer) {
-            var playerInputList = inputsService.getInputsListForPosition(updatedPlayer.position);
+            var playerInputList = getInputsForPlayer(updatedPlayer);
 
             if(playerGame.hasAllInputs(playerInputList) && !args.forceInputCalc) {
                 console.log('Player "' + updatedPlayer.name + '" has required inputs for week ' + playerGame.week + ', ' + playerGame.year + '.  Skipping...');
@@ -56,7 +74,7 @@ function buildAndSaveInputsForPlayer(player) {
 
 function showProjectionsForPlayerOverTime(player) {
     console.log('Projections for "' + player.name + ' of the ' + player.team);
-    return projectionsService.buildProjectionsForAllGames(player, inputsService.getInputsListForPosition(player.position), args.strategy)
+    return projectionsService.buildProjectionsForAllGames(player, getInputsForPlayer(player), args.strategy)
         .then(function displayProjections(projections) {
             projections.forEach(function displayProjection(projection) {
                 console.log('  Week ' + projection.game.week + ', ' + projection.game.year + ': ' + projection.projected + ' projected, ' + projection.actual + ' actual');
