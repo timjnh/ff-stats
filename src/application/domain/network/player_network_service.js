1344933edf6b510c6042fee657610d420b80fbdb
Tim Jones <tim.jones@meltwater.com>
2016-09-10 19:12:53 -0400

    chore: Changing some console.log calls to logger calls

--- src/application/domain/network/player_network_service.js
+++ src/application/domain/network/player_network_service.js
@@ -3,29 +3,30 @@
 var synaptic = require('synaptic'),
     trainingService = require('./training_service'),
     NetworkStrategyFactory = require('./strategies/network_strategy_factory'),
-    PlayerNetwork = require('./player_network');
+    PlayerNetwork = require('./player_network'),
+    logger = require('../../../lib/logger');
 
 function PlayerNetworkService() {}
 
 PlayerNetworkService.prototype.buildNetworkUpToGame = function buildNetworkUpToGame(player, game, inputs, strategy) {
     var startTime = new Date();
-    console.log('Building network for player "' + player.name + '" for ' + game.year + ', week ' + game.week + ' with inputs ' + inputs.join(', '));
+    logger.debug('Building network for player "' + player.name + '" for ' + game.year + ', week ' + game.week + ' with inputs ' + inputs.join(', '));
     return trainingService.getTrainingSetsForPlayerUpToGame(player, game, inputs)
         .then(function buildNetworkFromTrainingSets(trainingSets) {
             var network,
                 networkStrategy = NetworkStrategyFactory.createStrategy(strategy);
 
-            console.log('Training set with ' + trainingSets.length + ' items retrieved in ' + ((new Date()).getTime() - startTime.getTime()) + ' ms');
+            logger.debug('Training set with ' + trainingSets.length + ' items retrieved in ' + ((new Date()).getTime() - startTime.getTime()) + ' ms');
             startTime = new Date();
 
             if(!trainingSets.length) {
-                console.log('Could not build any training sets for player "' + player.name + '" in ' + game.year + ', week ' + game.week);
+                logger.error('Could not build any training sets for player "' + player.name + '" in ' + game.year + ', week ' + game.week);
                 return null;
             }
 
             network = networkStrategy.createAndTrainNetwork(trainingSets);
 
-            console.log('Network built in ' + ((new Date()).getTime() - startTime.getTime()) + ' ms');
+            logger.debug('Network built in ' + ((new Date()).getTime() - startTime.getTime()) + ' ms');
 
             return PlayerNetwork.create({
                 player: { name: player.name, team: player.team },
